4127cd4172d17c0b833747edf44af2f16cf19421
Chee Quee <mitchellcq@gmail.com>
2020-11-02 21:23:22 +1100

    no response from ask bcs, enjoy my C i guess

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-// ==============================================================================
 // DEPENDENCIES
 // Series of npm packages that we will use to give our server useful functionality
 // ==============================================================================
 
 var express = require("express");
 
-// ==============================================================================
 // EXPRESS CONFIGURATION
 // This sets up the basic properties for our express server
 // ==============================================================================
@@ -22,7 +20,6 @@ app.use("/assets", express.static("./assets"));
 require("./routing/html-routes")(app);
 require("./routing/api-routes")(app);
 
-// =============================================================================
 // LISTENER
 // The below code effectively "starts" our server
 // =============================================================================
